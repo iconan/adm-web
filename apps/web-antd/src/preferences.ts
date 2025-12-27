095cadc4fde499bd9ee159ee6510a7a87f679f07
mayday <yangchengcool@live.com>
2025-12-28 00:28:02 +0800

    dev

--- apps/web-antd/src/preferences.ts
+++ apps/web-antd/src/preferences.ts
@@ -9,7 +9,5 @@ export const overridesPreferences = defineOverridesPreferences({
   // overrides
   app: {
     name: import.meta.env.VITE_APP_TITLE,
-    transition: { enable: false },
-    theme: { mode: 'light' },
   },
 });
